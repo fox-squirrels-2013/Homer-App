7030f7fd8ddeb84d6650f102d856dd4baa99228e
Garreth Dottin <gd7627a@american.edu>
2013-11-30 13:10:44 -0800

    Removed Nav Bar

--- Resources/alloy/controllers/index.js
+++ Resources/alloy/controllers/index.js
@@ -6,30 +6,16 @@ function Controller() {
     arguments[0] ? arguments[0]["__itemTemplate"] : null;
     var $ = this;
     var exports = {};
-    var __defers = {};
     $.__views.index = Ti.UI.createWindow({
-        backgroundColor: "white",
+        backgroundColor: "#e0e0e0",
+        navBarHidden: true,
         id: "index"
     });
     $.__views.index && $.addTopLevelView($.__views.index);
-    $.__views.label = Ti.UI.createLabel({
-        width: Ti.UI.SIZE,
-        height: Ti.UI.SIZE,
-        color: "#000",
-        text: "Hello, World",
-        id: "label"
-    });
-    $.__views.index.add($.__views.label);
-    try {
-        $.__views.label.addEventListener("click", getDiscounts.doClick);
-    } catch (e) {
-        __defers["$.__views.label!click!getDiscounts.doClick"] = true;
-    }
     exports.destroy = function() {};
     _.extend($, $.__views);
-    var url = "http://shrouded-inlet-6169.herokuapp.com/example.json";
+    var url = "http://google.com";
     var json;
-    console.log("3rd thing");
     getDiscounts = {
         doClick: function() {
             var xhr = Ti.Network.createHTTPClient({
@@ -38,15 +24,11 @@ function Controller() {
                     $.label.text = json;
                 }
             });
-            console.log("4th thing");
             xhr.open("GET", url);
             xhr.send();
         }
     };
-    console.log("2nd thing");
     $.index.open();
-    console.log("1st");
-    __defers["$.__views.label!click!getDiscounts.doClick"] && $.__views.label.addEventListener("click", getDiscounts.doClick);
     _.extend($, exports);
 }
 
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,19 +1,17 @@
- 
- var url= 'http://shrouded-inlet-6169.herokuapp.com/example.json';
+ var url= 'http://google.com';
  var json;
- 
+  
    getDiscounts = { 
-  	 	
-    doClick: function(e) {
-      var xhr = Ti.Network.createHTTPClient({
-	  	onload: function (e) {	
-	    json = JSON.parse(this.responseText)
-	  	$.label.text = json;
-	 }	
-	});
-	  xhr.open("GET", url);
-	  xhr.send();	
- 	} 
+                    
+     doClick: function(e) {
+       var xhr = Ti.Network.createHTTPClient({
+        onload: function (e) {        
+        json = JSON.parse(this.responseText);
+        $.label.text = json;
+         }        
+         });
+          xhr.open("GET", url);
+          xhr.send();        
+          } 
 };
-
 $.index.open();
